0e118a9e674e6cbda93fad26321af05105dbfe6b
Sushant Poudel <sushantpaudel@gmail.com>
2020-08-03 15:29:53 +0545

    New Design Assets Added. Environment Created.

---
+++ public/vendor/bs-custom-file-input/src/selector.js
@@ -0,0 +1,8 @@
+const Selector = {
+    CUSTOMFILE: '.custom-file input[type="file"]',
+    CUSTOMFILELABEL: '.custom-file-label',
+    FORM: 'form',
+    INPUT: 'input',
+}
+
+export default Selector
